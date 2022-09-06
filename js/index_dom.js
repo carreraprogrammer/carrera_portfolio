e845339c0f2b40a1b1b2ac20d31508603f7b4c80
Alexandre Sales Medina Ferreira <94998770+alexmedinasf@users.noreply.github.com>
2022-09-06 12:34:42 -0300

    fix linters

--- js/index_dom.js
+++ js/index_dom.js
@@ -1,14 +1,12 @@
+const hamburger = document.querySelector('.burger-menu');
+const navMenu = document.querySelector('.mobile-menu');
 
-const hamburger = document.querySelector(".burger-menu");
-const navMenu = document.querySelector(".mobile-menu");
-
-hamburger.addEventListener("click", () => {
-    navMenu.classList.toggle("is-active");
-    hamburger.classList.toggle("is-active");
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('is-active');
+  hamburger.classList.toggle('is-active');
 });
 
-
-document.querySelectorAll(".nav-link").forEach(element => element.addEventListener("click", () => {
-    hamburger.classList.remove("is-active");
-    navMenu.classList.remove("is-active");
-}))
+document.querySelectorAll('.nav-link').forEach((element) => element.addEventListener('click', () => {
+  hamburger.classList.remove('is-active');
+  navMenu.classList.remove('is-active');
+}));
