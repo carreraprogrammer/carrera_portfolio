ab2476cf45fb3e2206dde41d708d92543a69d4f0
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-09-06 18:20:37 -0500

    Add javascript events

--- js/index_dom.js
+++ js/index_dom.js
@@ -1,3 +1,5 @@
+// This is the DOM for burger menu //
+
 const hamburger = document.querySelector('.burger-menu');
 const navMenu = document.querySelector('.mobile-menu');
 
@@ -10,3 +12,16 @@ document.querySelectorAll('.nav-link').forEach((element) => element.addEventList
   hamburger.classList.remove('is-active');
   navMenu.classList.remove('is-active');
 }));
+
+// Here start the dom for the preview of the projects //
+
+const previewProject = document.querySelector('.preview-project');
+const previewx = document.querySelector('.project-x');
+
+document.querySelectorAll('.preview-btn').forEach((element) => element.addEventListener('click', () => {
+  previewProject.classList.toggle('is-active');
+}));
+
+previewx.addEventListener('click', () => {
+  previewProject.classList.remove('is-active');
+});
