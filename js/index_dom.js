22d5e76949601694c2e04ca11a0b0cd3726a6fc9
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-09-07 17:55:49 -0500

    Add events for technologies tags

--- js/index_dom.js
+++ js/index_dom.js
@@ -19,6 +19,7 @@ document.querySelectorAll('.nav-link').forEach((element) => element.addEventList
 
 let projectNames = document.getElementsByClassName('card-title')
 let names = [].map.call(projectNames, elem => elem.textContent);
+console.log(names)
 let projectLongDescriptions = document.getElementsByClassName('long-description')
 let longDescription = [].map.call(projectLongDescriptions, elem => elem.textContent);
 let projectImages = document.getElementsByClassName('project-img')
@@ -28,24 +29,53 @@ let seeLiveLinks = [].map.call(projectSeeLive, elem => elem.href);
 let projectSeeSource = document.getElementsByClassName('see-source-link')
 let seeSourceLinks = [].map.call(projectSeeSource, elem => elem.href);
 
+// Here I create an array for each project tags and the preview tags
+
+let projectOneTags = document.getElementsByClassName('tags-one');
+let projectTwoTags = document.getElementsByClassName('tags-two');
+let projectThreeTags = document.getElementsByClassName('tags-three');
+let projectFourTags = document.getElementsByClassName('tags-four');
+let projectFiveTags = document.getElementsByClassName('tags-five');
+let projectSixTags = document.getElementsByClassName('tags-six');
+
+let tagsOne = [].map.call(projectOneTags, elem => elem.textContent);
+let tagsTwo = [].map.call(projectTwoTags, elem => elem.textContent);
+let tagsThree = [].map.call(projectThreeTags, elem => elem.textContent);
+let tagsFour = [].map.call(projectFourTags, elem => elem.textContent);
+let tagsFive = [].map.call(projectFiveTags, elem => elem.textContent);
+let tagsSix = [].map.call(projectSixTags, elem => elem.textContent);
+
+// I will create a function to create li elements //
+
+function createMenuItem(name) {
+  let li = document.createElement('li');
+  li.textContent = name;
+  return li;
+}
+
+let previewTags = document.querySelector('.preview-tags');
+
 // In this section, I create an Object constructor for each project individually //
 
-function Projects(name, description, img, seeLive, seeSource) {
+function Projects(name, description, img, seeLive, seeSource, tags) {
   this.name = name;
   this.longDescription = description;
   this.projectImage = img;
   this.seeLive = seeLive;
   this.seeSource = seeSource;
+  this.tags = tags;
 }
 
 // In this section, I use the object constructor to create objects of each project //
 
-let projectOne = new Projects(names[0], longDescription[0], projectImage[0], seeLiveLinks[0], seeSourceLinks[0]);
-let projectTwo = new Projects(names[1], longDescription[1], projectImage[1], seeLiveLinks[1], seeSourceLinks[1]);
-let projectThree = new Projects(names[2], longDescription[2], projectImage[2], seeLiveLinks[2], seeSourceLinks[2]);
-let projectFour = new Projects(names[3], longDescription[3], projectImage[3], seeLiveLinks[3], seeSourceLinks[3]);
-let projectFive = new Projects(names[4], longDescription[4], projectImage[4], seeLiveLinks[4],  seeSourceLinks[4]);
-let projectSix = new Projects(names[5], longDescription[5], projectImage[5], seeLiveLinks[5], seeSourceLinks[5]);
+let projectOne = new Projects(names[0], longDescription[0], projectImage[0], seeLiveLinks[0], seeSourceLinks[0], tagsOne);
+let projectTwo = new Projects(names[1], longDescription[1], projectImage[1], seeLiveLinks[1], seeSourceLinks[1], tagsTwo);
+let projectThree = new Projects(names[2], longDescription[2], projectImage[2], seeLiveLinks[2], seeSourceLinks[2], tagsThree);
+let projectFour = new Projects(names[3], longDescription[3], projectImage[3], seeLiveLinks[3], seeSourceLinks[3], tagsFour);
+let projectFive = new Projects(names[4], longDescription[4], projectImage[4], seeLiveLinks[4],  seeSourceLinks[4], tagsFive);
+let projectSix = new Projects(names[5], longDescription[5], projectImage[5], seeLiveLinks[5], seeSourceLinks[5], tagsSix);
+
+console.log(projectOne);
 
 // Here start the dom for the preview of the projects //
 
@@ -62,6 +92,9 @@ previewBtn.forEach((element) => element.addEventListener('click', () => {
     document.querySelector('.preview-project-img').src = projectOne.projectImage;
     document.querySelector('.preview-see-live').href = projectOne.seeLive;
     document.querySelector('.preview-see-source').href = projectOne.seeSource;
+    for (let i = 0; i < tagsOne.length; i++) {
+      previewTags.appendChild(createMenuItem(tagsOne[i]));
+      }
   } else if(element.classList.contains('project-two')){
     document.querySelector('.preview-project-title').textContent = projectTwo.name;
     document.querySelector('.preview-card-p').textContent = projectTwo.longDescription;
@@ -69,6 +102,9 @@ previewBtn.forEach((element) => element.addEventListener('click', () => {
     document.querySelector('.preview-project-img').src = projectTwo.projectImage;
     document.querySelector('.preview-see-live').href = projectTwo.seeLive;
     document.querySelector('.preview-see-source').href = projectTwo.seeSource;
+    for (let i = 0; i < tagsTwo.length; i++) {
+      previewTags.appendChild(createMenuItem(tagsTwo[i]));
+      };
   } else if(element.classList.contains('project-three')){
     document.querySelector('.preview-project-title').textContent = projectThree.name;
     document.querySelector('.preview-card-p').textContent = projectThree.longDescription;
@@ -76,6 +112,9 @@ previewBtn.forEach((element) => element.addEventListener('click', () => {
     document.querySelector('.preview-project-img').src = projectThree.projectImage;
     document.querySelector('.preview-see-live').href = projectThree.seeLive;
     document.querySelector('.preview-see-source').href = projectThree.seeSource;
+    for (let i = 0; i < tagsThree.length; i++) {
+      previewTags.appendChild(createMenuItem(tagsThree[i]));
+      };
   } else if(element.classList.contains('project-four')){
     document.querySelector('.preview-project-title').textContent = projectFour.name;
     document.querySelector('.preview-card-p').textContent = projectFour.longDescription;
@@ -83,6 +122,9 @@ previewBtn.forEach((element) => element.addEventListener('click', () => {
     document.querySelector('.preview-project-img').src = projectFour.projectImage;
     document.querySelector('.preview-see-live').href = projectFour.seeLive;
     document.querySelector('.preview-see-source').href = projectFour.seeSource;
+    for (let i = 0; i < tagsFour.length; i++) {
+      previewTags.appendChild(createMenuItem(tagsFour[i]));
+      };
   } else if(element.classList.contains('project-five')){
     document.querySelector('.preview-project-title').textContent = projectFive.name;
     document.querySelector('.preview-card-p').textContent = projectFive.longDescription;
@@ -90,6 +132,9 @@ previewBtn.forEach((element) => element.addEventListener('click', () => {
     document.querySelector('.preview-project-img').src = projectFive.projectImage;
     document.querySelector('.preview-see-live').href = projectFive.seeLive;
     document.querySelector('.preview-see-source').href = projectFive.seeSource;
+    for (let i = 0; i < tagsFive.length; i++) {
+      previewTags.appendChild(createMenuItem(tagsFive[i]));
+      };
   } else if(element.classList.contains('project-six')){
     document.querySelector('.preview-project-title').textContent = projectSix.name;
     document.querySelector('.preview-card-p').textContent = projectSix.longDescription;
@@ -97,11 +142,32 @@ previewBtn.forEach((element) => element.addEventListener('click', () => {
     document.querySelector('.preview-project-img').src = projectSix.projectImage;
     document.querySelector('.preview-see-live').href = projectSix.seeLive;
     document.querySelector('.preview-see-source').href = projectSix.seeSource;
+    for (let i = 0; i < tagsSix.length; i++) {
+      previewTags.appendChild(createMenuItem(tagsSix[i]));
+      };
   }
-}));
+})); 
 
 previewx.addEventListener('click', () => {
   previewProject.classList.remove('is-active');
+  setTimeout(() => {
+    document.location.reload();
+  }, 800);
 });
+
+// Here I create the daily selection tags
+
+let dailyTags = document.getElementsByClassName('daily-tags');
+let dailyBtn = document.querySelector('.see-more-btn');
+
+let tagsZero = [].map.call(dailyTags, elem => elem.textContent)
+
+console.log(tagsZero)
+
+dailyBtn.addEventListener('click', () => {
+  for (let i = 0; i < tagsZero.length; i++) {
+    previewTags.appendChild(createMenuItem(tagsZero[i]));
+    };
+  });
 
 
