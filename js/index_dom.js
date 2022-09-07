407fa23774fd586e1d5786890acb36c58099bcfd
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-09-07 18:59:54 -0500

    Fix linter errors

--- js/index_dom.js
+++ js/index_dom.js
@@ -13,11 +13,10 @@ document.querySelectorAll('.nav-link').forEach((element) => element.addEventList
   navMenu.classList.remove('is-active');
 }));
 
-//Here I will create the projects array to save the project data //
+// Here I will create the projects array to save the project data //
 
 const projectNames = document.getElementsByClassName('card-title');
 const names = [].map.call(projectNames, (elem) => elem.textContent);
-console.log(names)
 const projectLongDescriptions = document.getElementsByClassName('long-description');
 const longDescription = [].map.call(projectLongDescriptions, (elem) => elem.textContent);
 const projectImages = document.getElementsByClassName('project-img');
@@ -36,12 +35,12 @@ const projectFourTags = document.getElementsByClassName('tags-four');
 const projectFiveTags = document.getElementsByClassName('tags-five');
 const projectSixTags = document.getElementsByClassName('tags-six');
 
-const tagsOne = [].map.call(projectOneTags, elem => elem.textContent);
-const tagsTwo = [].map.call(projectTwoTags, elem => elem.textContent);
-const tagsThree = [].map.call(projectThreeTags, elem => elem.textContent);
-const tagsFour = [].map.call(projectFourTags, elem => elem.textContent);
-const tagsFive = [].map.call(projectFiveTags, elem => elem.textContent);
-const tagsSix = [].map.call(projectSixTags, elem => elem.textContent);
+const tagsOne = [].map.call(projectOneTags, (elem) => elem.textContent);
+const tagsTwo = [].map.call(projectTwoTags, (elem) => elem.textContent);
+const tagsThree = [].map.call(projectThreeTags, (elem) => elem.textContent);
+const tagsFour = [].map.call(projectFourTags, (elem) => elem.textContent);
+const tagsFive = [].map.call(projectFiveTags, (elem) => elem.textContent);
+const tagsSix = [].map.call(projectSixTags, (elem) => elem.textContent);
 
 // I will create a function to create li elements //
 
@@ -66,12 +65,18 @@ function Projects(name, description, img, seeLive, seeSource, tags) {
 
 // In this section, I use the object constructor to create objects of each project //
 
-const projectOne = new Projects(names[0], longDescription[0], projectImage[0], seeLiveLinks[0], seeSourceLinks[0], tagsOne);
-const projectTwo = new Projects(names[1], longDescription[1], projectImage[1], seeLiveLinks[1], seeSourceLinks[1], tagsTwo);
-const projectThree = new Projects(names[2], longDescription[2], projectImage[2], seeLiveLinks[2], seeSourceLinks[2], tagsThree);
-const projectFour = new Projects(names[3], longDescription[3], projectImage[3], seeLiveLinks[3], seeSourceLinks[3], tagsFour);
-const projectFive = new Projects(names[4], longDescription[4], projectImage[4], seeLiveLinks[4],  seeSourceLinks[4], tagsFive);
-const projectSix = new Projects(names[5], longDescription[5], projectImage[5], seeLiveLinks[5], seeSourceLinks[5], tagsSix);
+const projectOne = new Projects(names[0], longDescription[0], projectImage[0],
+seeLiveLinks[0], seeSourceLinks[0], tagsOne);
+const projectTwo = new Projects(names[1], longDescription[1], projectImage[1], 
+seeLiveLinks[1], seeSourceLinks[1], tagsTwo);
+const projectThree = new Projects(names[2], longDescription[2], projectImage[2], 
+seeLiveLinks[2], seeSourceLinks[2], tagsThree);
+const projectFour = new Projects(names[3], longDescription[3], projectImage[3], 
+seeLiveLinks[3], seeSourceLinks[3], tagsFour);
+const projectFive = new Projects(names[4], longDescription[4], projectImage[4], 
+seeLiveLinks[4],  seeSourceLinks[4], tagsFive);
+const projectSix = new Projects(names[5], longDescription[5], projectImage[5], 
+seeLiveLinks[5], seeSourceLinks[5], tagsSix);
 
 // Here start the dom for the preview of the projects //
 
@@ -81,17 +86,17 @@ const previewBtn = document.querySelectorAll('.preview-btn');
 
 previewBtn.forEach((element) => element.addEventListener('click', () => {
   previewProject.classList.toggle('is-active');
-  if(element.classList.contains('project-one')){
+  if (element.classList.contains('project-one')) {
     document.querySelector('.preview-project-title').textContent = projectOne.name;
     document.querySelector('.preview-card-p').textContent = projectOne.longDescription;
     document.querySelector('.mobile-preview-card-p').textContent = projectOne.longDescription;
     document.querySelector('.preview-project-img').src = projectOne.projectImage;
     document.querySelector('.preview-see-live').href = projectOne.seeLive;
     document.querySelector('.preview-see-source').href = projectOne.seeSource;
-    for (let i = 0; i < tagsOne.length; i++) {
+    for (let i = 0; i < tagsOne.length; i += 1) {
       previewTags.appendChild(createMenuItem(tagsOne[i]));
-      }
-  } else if(element.classList.contains('project-two')){
+    }
+  } else if (element.classList.contains('project-two')) {
     document.querySelector('.preview-project-title').textContent = projectTwo.name;
     document.querySelector('.preview-card-p').textContent = projectTwo.longDescription;
     document.querySelector('.mobile-preview-card-p').textContent = projectTwo.longDescription;
@@ -101,17 +106,17 @@ previewBtn.forEach((element) => element.addEventListener('click', () => {
     for (let i = 0; i < tagsTwo.length; i++) {
       previewTags.appendChild(createMenuItem(tagsTwo[i]));
       };
-  } else if(element.classList.contains('project-three')){
+  } else if (element.classList.contains('project-three')) {
     document.querySelector('.preview-project-title').textContent = projectThree.name;
     document.querySelector('.preview-card-p').textContent = projectThree.longDescription;
     document.querySelector('.mobile-preview-card-p').textContent = projectThree.longDescription;
     document.querySelector('.preview-project-img').src = projectThree.projectImage;
     document.querySelector('.preview-see-live').href = projectThree.seeLive;
     document.querySelector('.preview-see-source').href = projectThree.seeSource;
-    for (let i = 0; i < tagsThree.length; i++) {
+    for (let i = 0; i < tagsThree.length; i += 1) {
       previewTags.appendChild(createMenuItem(tagsThree[i]));
       };
-  } else if(element.classList.contains('project-four')){
+  } else if (element.classList.contains('project-four')) {
     document.querySelector('.preview-project-title').textContent = projectFour.name;
     document.querySelector('.preview-card-p').textContent = projectFour.longDescription;
     document.querySelector('.mobile-preview-card-p').textContent = projectFour.longDescription;
@@ -121,24 +126,24 @@ previewBtn.forEach((element) => element.addEventListener('click', () => {
     for (let i = 0; i < tagsFour.length; i++) {
       previewTags.appendChild(createMenuItem(tagsFour[i]));
       };
-  } else if(element.classList.contains('project-five')){
+  } else if (element.classList.contains('project-five')) {
     document.querySelector('.preview-project-title').textContent = projectFive.name;
     document.querySelector('.preview-card-p').textContent = projectFive.longDescription;
     document.querySelector('.mobile-preview-card-p').textContent = projectFive.longDescription;
     document.querySelector('.preview-project-img').src = projectFive.projectImage;
     document.querySelector('.preview-see-live').href = projectFive.seeLive;
     document.querySelector('.preview-see-source').href = projectFive.seeSource;
-    for (let i = 0; i < tagsFive.length; i++) {
+    for (let i = 0; i < tagsFive.length; i += 1) {
       previewTags.appendChild(createMenuItem(tagsFive[i]));
       };
-  } else if(element.classList.contains('project-six')){
+  } else if (element.classList.contains('project-six')) {
     document.querySelector('.preview-project-title').textContent = projectSix.name;
     document.querySelector('.preview-card-p').textContent = projectSix.longDescription;
     document.querySelector('.mobile-preview-card-p').textContent = projectSix.longDescription;
     document.querySelector('.preview-project-img').src = projectSix.projectImage;
     document.querySelector('.preview-see-live').href = projectSix.seeLive;
     document.querySelector('.preview-see-source').href = projectSix.seeSource;
-    for (let i = 0; i < tagsSix.length; i++) {
+    for (let i = 0; i < tagsSix.length; i += 1) {
       previewTags.appendChild(createMenuItem(tagsSix[i]));
       };
   }
@@ -156,14 +161,10 @@ previewx.addEventListener('click', () => {
 const dailyTags = document.getElementsByClassName('daily-tags');
 const dailyBtn = document.querySelector('.see-more-btn');
 
-const tagsZero = [].map.call(dailyTags, (elem) => elem.textContent)
-
-console.log(tagsZero)
+const tagsZero = [].map.call(dailyTags, (elem) => elem.textContent);
 
 dailyBtn.addEventListener('click', () => {
-  for (let i = 0; i < tagsZero.length; i++) {
+  for (let i = 0; i < tagsZero.length; i += 1) {
     previewTags.appendChild(createMenuItem(tagsZero[i]));
-    };
-  });
-
-
+  };
+});
