fb157765020a6040c86c5075081a9a86d4153354
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-09-09 17:46:32 -0500

    fix linter errors

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,34 +1,32 @@
 const form = document.querySelector('form');
 const email = document.getElementById('email');
-const error = email.nextElementSibling; 
+const error = email.nextElementSibling;
 
-const emailSearch = /[A-Z]/g
+const emailSearch = /[A-Z]/g;
 
 email.addEventListener('input', () => {
-    const isInvalid = emailSearch.test(email.value);
+  const isInvalid = emailSearch.test(email.value);
 
-    if (isInvalid) {
-      email.classList.add("invalid");
-    } else {
-      email.classList.remove("invalid");
-      error.textContent = "";
-      error.className = "error";
-    }
+  if (isInvalid) {
+    email.classList.add('invalid');
+  } else {
+    email.classList.remove('invalid');
+    error.textContent = '';
+    error.className = 'error';
+  }
 });
 
-form.addEventListener("submit", () => {
-    event.preventDefault();
+form.addEventListener('submit', () => {
+  event.preventDefault();
 
-    const isInvalid = emailSearch.test(email.value);
-    if (isInvalid){
-      email.classList.add("invalid");
-      error.textContent = `Please, enter a lowercase email. Try with: "${email.value.toLowerCase()}"`;
-      error.className = "error active";
-    } else {
-      email.classList.toggle("valid");
-      error.textContent = "";
-      error.className = "error"; 
-    }
+  const isInvalid = emailSearch.test(email.value);
+  if (isInvalid){
+    email.classList.add('invalid');
+    error.textContent = `Please, enter a lowercase email. Try with: '${email.value.toLowerCase()}'`;
+    error.className = 'error active';
+  } else {
+    email.classList.toggle('valid');
+    error.textContent = '';
+    error.className = 'error';
+  }
 });
-
-
