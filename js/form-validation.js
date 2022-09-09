eca5d0ab4bb206b7199770dbf2fbadabf6da8cc3
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-09-09 17:22:26 -0500

    add form validation

---
+++ js/form-validation.js
@@ -0,0 +1,34 @@
+const form = document.querySelector('form');
+const email = document.getElementById('email');
+const error = email.nextElementSibling; 
+
+const emailSearch = /[A-Z]/g
+
+email.addEventListener('input', () => {
+    const isInvalid = emailSearch.test(email.value);
+
+    if (isInvalid) {
+      email.classList.add("invalid");
+    } else {
+      email.classList.remove("invalid");
+      error.textContent = "";
+      error.className = "error";
+    }
+});
+
+form.addEventListener("submit", () => {
+    event.preventDefault();
+
+    const isInvalid = emailSearch.test(email.value);
+    if (isInvalid){
+      email.classList.add("invalid");
+      error.textContent = `Please, enter a lowercase email. Try with: "${email.value.toLowerCase()}"`;
+      error.className = "error active";
+    } else {
+      email.classList.toggle("valid");
+      error.textContent = "";
+      error.className = "error"; 
+    }
+});
+
+
