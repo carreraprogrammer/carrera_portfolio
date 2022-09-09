5537563797ad1af8a7792175ac7e85c53ae3c6d7
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-09-09 18:59:39 -0500

    Fix submit and botton position

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,6 +1,6 @@
 const form = document.querySelector('form');
 const email = document.getElementById('email');
-const error = email.nextElementSibling;
+const error = document.getElementById('error');
 
 const emailSearch = /[A-Z]/g;
 
@@ -10,20 +10,22 @@ email.addEventListener('input', () => {
   if (isInvalid) {
     email.classList.add('invalid');
   } else {
+    error.classList.remove('invalid');
     error.textContent = '';
     error.className = 'error';
   }
 });
 
 form.addEventListener('submit', (event) => {
-  event.preventDefault();
+
   const isInvalid = emailSearch.test(email.value);
   if (isInvalid) {
     email.classList.add('invalid');
     error.textContent = `Please, enter a lowercase email. Try with: '${email.value.toLowerCase()}'`;
     error.className = 'error active';
+    event.preventDefault();
   } else {
-    email.classList.toggle('valid');
+    email.classList.remove('invalid');
     error.textContent = '';
     error.className = 'error';
   }
